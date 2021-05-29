7175ac928e1c731adaa419bfd51be5b3970384a3
Sanjay1082 <sanjay.ece2010@gmail.com>
2021-05-29 08:57:37 +0530

    Removed the print statement

--- src/test/java/stepDefiniations/placeAPIValidationsstep.java
+++ src/test/java/stepDefiniations/placeAPIValidationsstep.java
@@ -80,7 +80,6 @@ public class placeAPIValidationsstep {
     @Given("^DeletePlace Payload$")
     public void deleteplace_Payload() throws IOException{
     	ReqSpec = given().spec(Utils.requestSpec()).body(AddAPIPayload.DeletePlacePayLoad(place_id));
-    	System.out.println();
     }
     
   }
